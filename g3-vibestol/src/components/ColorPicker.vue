d4e6ae8da0349ff2925a5fdf6cb176e9118bc2dd
WilSonS9 <54662723+WilSonS9@users.noreply.github.com>
2020-01-20 10:27:07 +0100

    Fetch
    Added Fetch prototype

--- g3-vibestol/src/components/ColorPicker.vue
+++ g3-vibestol/src/components/ColorPicker.vue
@@ -13,14 +13,29 @@
 </template>
 
 <script>
+const axios = require("axios");
 import ColorPicker from "@radial-color-picker/vue-color-picker";
 import { mapGetters, mapMutations } from "vuex";
 
 export default {
+  created() {
+    this.fetch();
+  },
   components: { ColorPicker },
   data: () => ({}),
   methods: {
-    ...mapMutations(["updateColor"])
+    ...mapMutations(["updateColor"]),
+    async fetch() {
+      let green = false;
+      const {data} = await axios.get("https://03asg5lb76.execute-api.us-east-1.amazonaws.com/V1/stol?stol=VibeChair");
+      if(data.Status == "Grön") {
+        green = true;
+      } else {
+        green = false;
+      }
+      /* eslint-disable */
+      console.log(green);
+    }
   },
   computed: {
     ...mapGetters(["color", "rgb"])
