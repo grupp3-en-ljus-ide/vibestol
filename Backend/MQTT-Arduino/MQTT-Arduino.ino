f9d717aeafa21b41ee9d1fb5b551b89a42638724
abbsamsta <54816284+abbsamsta@users.noreply.github.com>
2020-02-19 08:26:26 +0100

    added rainbow mode arduino

--- Backend/MQTT-Arduino/MQTT-Arduino.ino
+++ Backend/MQTT-Arduino/MQTT-Arduino.ino
@@ -21,21 +21,39 @@ EspMQTTClient client(
 #define GREEN_Led 12 // Output D6 NodeMCU - Green LED
 #define BLUE_Led 13 // Output D7 NodeMCU - Blue LED
 
+typedef enum Effects{
+  StaticEffect,
+  RainbowEffect
+};
+Effects Effect;
+
 String Sub = "g3.vibestol@gmail.com/R";
 
 int rValue = 0;
 int gValue = 0;
 int bValue = 0;
+int effectValue = 1;
 
 void onConnectionEstablished() {
   client.subscribe(Sub, [] (const String & payload) {
     rValue = payload.substring(0, 3).toInt() * 4;
     gValue = payload.substring(3, 6).toInt() * 4;
-    bValue = payload.substring(6).toInt() * 4;
+    bValue = payload.substring(6, 9).toInt() * 4;
+    effectValue = payload.substring(9).toInt();
   });
 
-  setColor(rValue, gValue, bValue);
-
+  if (effectValue == 1) {
+      Effect = StaticEffect;
+  }
+  else if (effectValue == 2) {
+    rValue = 1000;
+    gValue = 0;
+    bValue = 0;
+    Effect = RainbowEffect;
+  }
+  else {
+    Serial.println("Unknown effect");
+  }
 }
 
 
@@ -43,6 +61,7 @@ void setColor(int r_value, int g_value, int b_value) {
   analogWrite(RED_Led, r_value);
   analogWrite(GREEN_Led, g_value);
   analogWrite(BLUE_Led, b_value);
+  
 }
 
 
@@ -51,10 +70,40 @@ void setup() {
   pinMode(GREEN_Led, OUTPUT);
   pinMode(BLUE_Led, OUTPUT);
   setColor(0, 0, 0);
-  // Serial.begin(115200);
+  Effect = StaticEffect;
+  Serial.begin(115200);
 }
 
 void loop() {
   client.loop();
   onConnectionEstablished();
+  switch (Effect) {
+    case StaticEffect:
+      setColor(rValue, gValue, bValue);
+      break;
+    case RainbowEffect:
+      if (rValue >= 1000 and gValue < 1000 and bValue == 0) {
+        gValue += 100;
+      }
+      else if(rValue > 0 and gValue >= 1000 and bValue == 0) {
+        rValue -= 100;
+      }
+      else if (rValue == 0 and gValue >= 1000 and bValue < 1000) {
+        bValue += 100;
+      }
+      else if(rValue == 0 and gValue > 0 and bValue >= 1000) {
+        gValue -= 100;
+      }
+      else if (rValue < 1000 and gValue == 0 and bValue >= 1000) {
+        rValue += 100;
+      }
+      else if(rValue >= 1000 and gValue == 0 and bValue > 0) {
+        bValue -= 100;
+      }
+      else {
+        Serial.println("Rainbow effect error");
+      }
+      setColor(rValue, gValue, bValue);
+      break;
+  }
 }
